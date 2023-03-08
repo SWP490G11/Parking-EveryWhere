2183bf7220904a2a76835caa35e3136e5cc361a8
Najumy <najumy1807.2k@gmail.com>
2023-03-08 14:45:08 +0700

    profile

--- front-end/src/pages/user/UserProfile.jsx
+++ front-end/src/pages/user/UserProfile.jsx
@@ -2,7 +2,7 @@ import "../../style/profile.css";
 import HeaderComp from "../../components/HeaderComp";
 import FilterComp from "../../components/FilterComp";
 import { UserOutlined } from "@ant-design/icons";
-import { Avatar, Space, AutoComplete, DatePicker, Button } from "antd";
+import { Avatar, Space, AutoComplete, DatePicker, Button,Radio } from "antd";
 import { useState } from "react";
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
@@ -22,6 +22,11 @@ export function UserProfile() {
       }));
     }
     setOptions(res);
+  };
+  const [value, setValue] = useState(1);
+  const onChange = (e) => {
+    console.log("radio checked", e.target.value);
+    setValue(e.target.value);
   };
   return (
     <>
@@ -44,7 +49,7 @@ export function UserProfile() {
             Full name:
             <AutoComplete
               style={{
-                marginLeft: "30px",
+                marginLeft: "20px",
                 width: 200,
               }}
               onSearch={handleSearch}
@@ -55,18 +60,26 @@ export function UserProfile() {
             Date of birth:
             <DatePicker
               style={{
-                marginLeft: "30px",
+                marginLeft: "20px",
                 width: 200,
               }}
               defaultValue={dayjs("2015-06-06", dateFormat)}
               enable
             />
           </li>
+
+          <li id="gender">
+            Gender
+            <Radio.Group onChange={onChange} value={value}>
+              <Radio value={1}>Male</Radio>
+              <Radio value={2}>Female</Radio>
+            </Radio.Group>
+          </li>
           <li id="address">
             Address
             <AutoComplete
               style={{
-                marginLeft: "30px",
+                marginLeft: "20px",
                 width: 200,
               }}
               onSearch={handleSearch}
@@ -77,7 +90,7 @@ export function UserProfile() {
             CMND
             <AutoComplete
               style={{
-                marginLeft: "30px",
+                marginLeft: "20px",
                 width: 200,
               }}
               onSearch={handleSearch}
@@ -88,7 +101,7 @@ export function UserProfile() {
             Phone number
             <AutoComplete
               style={{
-                marginLeft: "30px",
+                marginLeft: "20px",
                 width: 200,
               }}
               onSearch={handleSearch}
@@ -100,7 +113,7 @@ export function UserProfile() {
             Membership
             <AutoComplete
               style={{
-                marginLeft: "30px",
+                marginLeft: "20px",
                 width: 200,
               }}
               onSearch={handleSearch}
@@ -111,7 +124,7 @@ export function UserProfile() {
             Role
             <AutoComplete
               style={{
-                marginLeft: "30px",
+                marginLeft: "20px",
                 width: 200,
               }}
               onSearch={handleSearch}
@@ -119,52 +132,6 @@ export function UserProfile() {
             />
           </li>
         </ul>
-
-        <div id="account-information">
-          <h1 style={{ marginLeft: "50px" }}>Change password</h1>
-          <li id="username">
-            Username
-            <AutoComplete
-              style={{
-                marginLeft: "30px",
-                width: 200,
-              }}
-              onSearch={handleSearch}
-              options={options}
-            />
-          </li>
-          <li id="password">
-            Password
-            <AutoComplete
-              style={{
-                marginLeft: "30px",
-                width: 200,
-              }}
-              onSearch={handleSearch}
-              options={options}
-            />
-          </li>
-          <li id="password-re">
-            Re-password
-            <AutoComplete
-              style={{
-                marginLeft: "30px",
-                width: 200,
-              }}
-              onSearch={handleSearch}
-              options={options}
-            />
-          </li>
-          <Button
-            style={{
-              marginLeft: "5vw",
-              marginTop: "30px",
-            }}
-            type="primary"
-          >
-            Save change
-          </Button>
-        </div>
       </div>
       <FooterComp />
     </>
