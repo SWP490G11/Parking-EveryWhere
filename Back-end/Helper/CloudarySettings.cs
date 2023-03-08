558adedbf6f9b4806eb47f6b9e7b943863e5836e
Phúc <93722448+PhucNood@users.noreply.github.com>
2023-03-09 00:53:54 +0700

    Add new migration and upload image function

---
+++ Back-end/Helper/CloudarySettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Back_end.Helper
+{
+    public class CloudarySettings
+    {
+        public string CloudName { get; set; }
+
+        public string ApiKey { get; set; }
+
+        public string ApiSecret { get; set; }
+    }
+}
--- Back-end/Models/ImageModel.cs
+++ Back-end/Models/ImageModel.cs
@@ -2,5 +2,12 @@
 {
     public class ImageModel
     {
+
+        public string URL { get; set; }
+
+        public string PublicID { get; set; }
+
+        public bool IsMain { get; set; }
+
     }
 }
