38d4dce735c53f4e8891d3e7767934758b69fb2f
Akers0101 <dtphong010120@gmail.com>
2023-05-05 03:50:08 +0700

    udate

--- front-end/src/config/index.js
+++ front-end/src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
     API_URL: 'https://pew490.xyz/',
-    GOOGLE_API: 'AIzaSyDWTx7bREpM5B6JKdbzOvMW-RRlhkukmVE',
+    GOOGLE_API: 'AIzaSyCbll8tLBXWpHeb0VDuCTLi8NVwWG7Y7Hs',
     UPLOAD_URL: 'https://thumbsnap.com/api/upload',
     UPLOAD_API: '00003d8d43b50caff42940f2756a800e',
     DEFAULT_IMG_URL: 'https://avatars.githubusercontent.com/u/63761141?s=120&v=4'
